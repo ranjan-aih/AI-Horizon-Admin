484c04f849406f0f7ccaecaf68039e397d4df5b7
Ranjan Kumar Chauhan <ranjan@ai-horizon.io>
2025-07-23 11:16:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-[832px]'>
+        Admin Panel
+      </div>
+    </>
+  );
+}
+
+export default App;
